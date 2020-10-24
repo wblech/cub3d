8886674abd54c85bdfdb5d9ce14b4b16fa5dba53
Wincenty Bertoni Lech <wbertoni@student.42sp.org.br>
2020-10-24 18:08:59 -0300

    Add game struct

--- src/main.c
+++ src/main.c
@@ -2,28 +2,57 @@
 #include "cubfile.h"
 #include "cub3d.h"
 
+typedef struct s_game
+{
+	void *mlx_ptr;
+	void *win_ptr;
+} t_game;
+
+int ft_create_game(t_game *game, t_file cubfile)
+{
+	game->mlx_ptr = mlx_init();
+	game->win_ptr = mlx_new_window(game->mlx_ptr, cubfile.width,
+									cubfile.height, "Map");
+	if (!game->mlx_ptr|| !game->win_ptr)
+		return (0);
+	return (1);
+}
 
 
+
+int ft_setup(t_game *game, t_file cubfile)
+{
+	// g_minimap = FALSE;
+	if (!ft_create_game(game, cubfile))
+		return (0);
+	// if (!ft_raycast(game))
+	// 	return (0);
+	// // ft_cast_sprite(game);
+	// ft_draw_2d_map(game, game->img);
+	// ft_draw_3d_map(game, game->img);
+	// ft_draw_sprite(game, game->img);
+	// mlx_put_image_to_window(game->mlx, game->win, game->img->img, 0, 0);
+	return (1);
+}
 
 
 int	main(int argc, char **argv)
 {
-	void *mlx;
-	void *window_ptr;
 	t_file *cubfile;
 	t_img img;
+	t_game game;
 
+	cubfile = ft_cubfile(argv[1]);
 	if (argc < 2)
 	{
 		ft_putstr("Error:\nSecond argument must be a path to the map");
 		return (1);
 	}
+	if (!ft_setup(&game, *cubfile))
+		exit(1);
 
-	cubfile = ft_cubfile(argv[1]);
-	mlx = mlx_init();
-	window_ptr = mlx_new_window(mlx, (*cubfile).width, (*cubfile).height, "Testando Novamente");
-	img.img = mlx_new_image(mlx, (*cubfile).width, (*cubfile).height);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+	img.img = mlx_new_image(game.mlx_ptr, (*cubfile).width, (*cubfile).height);
+	img.addr = mlx_get_data_addr((&game)->win_ptr, &img.bits_per_pixel,
 									&img.line_length, &img.endian);
 
 	int i = 0;
@@ -35,9 +64,9 @@ int	main(int argc, char **argv)
 		i++;
 	}
 
-	mlx_put_image_to_window(mlx, window_ptr, img.img, 0, 0);
+	mlx_put_image_to_window(game.mlx_ptr, game.win_ptr , img.img, 0, 0);
 
-	mlx_loop(mlx); //tem que ficar na main
+	mlx_loop(game.mlx_ptr); //tem que ficar na main
 
 	// ft_del_file(cubfile);
 	// mlx_destroy_window(mlx, window_ptr);
